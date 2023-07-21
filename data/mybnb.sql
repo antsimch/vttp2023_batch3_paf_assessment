ed85c267b6ce8d287be74842b3fad2cfea485ce9
antsimch <anthonysimch@gmail.com>
2023-07-21 16:59:57 +0800

    sql and mongoimport

---
+++ data/mybnb.sql
@@ -0,0 +1,20 @@
+create database mybnb;
+
+use mybnb;
+
+create table acc_occupancy (
+    acc_id		varchar(10) not null,
+    vacancy		int,
+    constraint pk_acc_occupancy_id primary key(acc_id)
+);
+
+create table reservations (
+    resv_id			varchar(8) not null,
+    name			varchar(128) not null,
+    email			varchar(128),
+    acc_id			varchar(10) not null,
+    arrival_date	date,
+    duration		int,
+    constraint pk_reservations_id primary key(resv_id),
+    constraint fk_acc_id foreign key(acc_id) references acc_occupancy(acc_id)
+);
